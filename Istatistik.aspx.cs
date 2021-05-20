01ec28a33bbe9e34dc12e991bb296da8ed614e42
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-20 16:53:46 +0300

    istatistiklerle ilgili işlemler

---
+++ Istatistik.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace DynamicCV
+{
+    public partial class Istatistik : System.Web.UI.Page
+    {
+        private DbCVEntities db = new DbCVEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var maxTalent = db.TBL_YETENEKLER.Max(y => y.DERECE);
+
+            Label1.Text = db.TBL_YETENEKLER.Count().ToString();
+            Label2.Text = db.TBL_ILETISIM.Count().ToString();
+            Label3.Text = Math.Round((decimal)db.TBL_YETENEKLER.Average(x => x.DERECE), 2).ToString();
+            Label4.Text = maxTalent.ToString();
+            Label5.Text = db.TBL_YETENEKLER.Where(x=>x.DERECE == maxTalent).FirstOrDefault().YETENEK;
+        }
+    }
+}
